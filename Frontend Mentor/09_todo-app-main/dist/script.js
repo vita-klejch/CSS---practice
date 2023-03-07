3e626143df02c0f5f7dbf8d83fffe7d7a0fd887f
vita-klejch <vita9@post.cz>
2023-03-07 18:14:26 +0100

    completed challenge from Frontend Mentor
    challange name: todo-app
    source:  https://www.frontendmentor.io/challenges/todo-app-Su1_KokOW
    languages: HTML, CSS (SCSS), Java Script/Type Script
    tools used: flexbox, localstorage

---
+++ Frontend Mentor/09_todo-app-main/dist/script.js
@@ -0,0 +1,226 @@
+"use strict";
+// INFO: CSS class 'item--animation' is used for animating
+// when todo is added, deleted or filtered
+// GLOBAL VARIABLES
+let topId = 0; // keep track about TOP TODO ID - for creating new TODOs
+let toDoData = []; // stores all displayed TODO
+// GLOBAL CONSTANTS
+// location, where all TODO should be displayed
+const todoList = document.querySelector(".js-todo-list");
+// location, where will be displayed quantity of unfinnished TODOs
+let counterItem;
+// call init function - load data, display them
+populateItems();
+// FILTERS
+//
+const filterBtn = document.querySelectorAll("[data-filter]");
+let activeFilterName = "all";
+// ADD EVENTLISTENERS to filter BTN
+filterBtn.forEach((btn) => {
+    const filterType = btn.dataset.filter;
+    btn.addEventListener("click", () => {
+        handleFilterDisplay(filterType);
+        handleFilter(filterType);
+    });
+});
+// manage to display which filter is active - adds/removes class
+function handleFilterDisplay(newFilterName) {
+    filterBtn.forEach((btn) => {
+        const filterType = btn.dataset.filter;
+        filterType == activeFilterName &&
+            btn.classList.remove("item__filter--active");
+        filterType == newFilterName && btn.classList.add("item__filter--active");
+    });
+    activeFilterName = newFilterName;
+}
+// apply filters - HIDE/DISPLAY items
+function handleFilter(filterType) {
+    const displayedData = [
+        ...document.querySelectorAll(".js-todo-list .item:not(:last-child)"),
+    ];
+    if (filterType == "all") {
+        displayedData.forEach((item) => item.classList.remove("item--animation"));
+    }
+    else if (filterType == "active") {
+        displayedData.forEach((item) => {
+            if (item.classList.contains("item--checked")) {
+                item.classList.add("item--animation");
+            }
+            else {
+                item.classList.remove("item--animation");
+            }
+        });
+    }
+    else {
+        displayedData.forEach((item) => {
+            if (item.classList.contains("item--checked")) {
+                item.classList.remove("item--animation");
+            }
+            else {
+                item.classList.add("item--animation");
+            }
+        });
+    }
+}
+// DISPLAY DATA, POPULATE
+// LOAD data from localstorage and returns them
+function loadData() {
+    const JSONdata = localStorage.getItem("todoData");
+    const loadedData = JSONdata && JSON.parse(JSONdata);
+    toDoData = loadedData;
+    if (toDoData) {
+        toDoData.forEach((item) => {
+            item.id > topId && (topId = item.id);
+        });
+    }
+    else {
+        toDoData = [];
+    }
+    return loadedData;
+}
+// SAVE data to localstorage
+function saveData(dataToSave) {
+    localStorage.setItem("todoData", JSON.stringify(dataToSave));
+}
+// displayData given data
+function displayData(dataToDisplay) {
+    var _a;
+    if (dataToDisplay) {
+        dataToDisplay.forEach((itemData) => {
+            const itemComponent = getItemComponent(itemData);
+            todoList === null || todoList === void 0 ? void 0 : todoList.appendChild(itemComponent);
+        });
+    }
+    const filterArea = ((_a = document.querySelector(".js-filter-area")) === null || _a === void 0 ? void 0 : _a.children[0]);
+    // adds second filter area
+    // - it is displayed on different screen size (tablets and bigger)
+    const filterClone = filterArea === null || filterArea === void 0 ? void 0 : filterArea.cloneNode(true);
+    filterClone.classList.add("tablet-display");
+    filterClone.classList.remove("item");
+    // adds info area
+    const infoArea = getStyledElement("div", "item");
+    infoArea.classList.add("item__info-area");
+    counterItem = getStyledElement("div", "item__title", "X");
+    counterItem.classList.add("js-todo-counter");
+    const clearItem = getStyledElement("div", "item__title", "Clear completed");
+    clearItem.classList.add("item--clickable");
+    clearItem.addEventListener("click", clearCompleted);
+    infoArea.appendChild(counterItem);
+    infoArea.appendChild(filterClone);
+    infoArea.appendChild(clearItem);
+    todoList === null || todoList === void 0 ? void 0 : todoList.appendChild(infoArea);
+}
+// INIT - calls loading func. and displays returned data
+function populateItems() {
+    const data = loadData();
+    displayData(data);
+    counterItem = document.querySelector(".js-todo-counter");
+    refreshCounter();
+}
+// helper function, returns element with css class
+function getStyledElement(element, classStyle, textContent = null) {
+    const component = document.createElement(element);
+    component.classList.add(classStyle);
+    textContent && component.appendChild(document.createTextNode(textContent));
+    return component;
+}
+// returns item component
+function getItemComponent(itemData, isNewComponent = false) {
+    let itemElement = getStyledElement("div", "item");
+    itemData.isCompleted && itemElement.classList.add("item--checked");
+    itemElement.setAttribute("data-item-id", `${itemData.id}`);
+    let checkboxElement = getStyledElement("div", "item__checkbox");
+    checkboxElement.addEventListener("click", () => toggleCompleteStatus(itemElement));
+    let titleElement = getStyledElement("div", "item__title", itemData.title);
+    let deleteElement = getStyledElement("div", "item__delete");
+    deleteElement.addEventListener("click", () => deleteItem(itemElement));
+    itemElement.appendChild(checkboxElement);
+    itemElement.appendChild(titleElement);
+    itemElement.appendChild(deleteElement);
+    // handles animation when adding NEW item
+    if (isNewComponent) {
+        itemElement.classList.add("item--animation");
+    }
+    return itemElement;
+}
+// helper - finds index of item in array of todo data
+function getIndexOfID(id) {
+    let index = toDoData.map((item) => item.id).indexOf(id);
+    return index;
+}
+// deletes given item
+function deleteItem(itemToDelete) {
+    const IDtoDelete = parseInt(itemToDelete.dataset.itemId);
+    const pos = getIndexOfID(IDtoDelete);
+    toDoData.splice(pos, 1);
+    saveData(toDoData);
+    // add animation for DELETED item
+    itemToDelete.classList.add("item--animation");
+    setTimeout(() => {
+        removeElement(itemToDelete);
+    }, 1000);
+    refreshCounter();
+}
+// toggles check/uncheck status for component/TODO
+function toggleCompleteStatus(itemToToggle) {
+    const id = parseInt(itemToToggle.dataset.itemId);
+    const pos = getIndexOfID(id);
+    toDoData[pos].isCompleted = !toDoData[pos].isCompleted;
+    saveData(toDoData);
+    itemToToggle.classList.toggle("item--checked");
+    refreshCounter();
+}
+// DELETE element from DOM
+function removeElement(elementToRemove) {
+    while (elementToRemove.firstChild) {
+        elementToRemove.removeChild(elementToRemove.firstChild);
+    }
+    elementToRemove.remove();
+    refreshCounter();
+}
+// handle INPUT - add new TODO
+const todoInput = document.querySelector(".js-todo-input");
+document.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addNewTodo(todoInput.value);
+    todoInput && (todoInput.value = "");
+});
+function addNewTodo(todoTitle) {
+    const newTodo = {
+        id: ++topId,
+        title: todoTitle,
+        isCompleted: false,
+    };
+    const newTodoItem = getItemComponent(newTodo, true);
+    todoList === null || todoList === void 0 ? void 0 : todoList.insertBefore(newTodoItem, todoList.lastChild);
+    toDoData = [...toDoData, newTodo];
+    saveData(toDoData);
+    setTimeout(() => newTodoItem.classList.remove("item--animation"), 50);
+    refreshCounter();
+}
+function clearCompleted() {
+    const displayedData = [
+        ...document.querySelectorAll(".js-todo-list .item:not(:last-child)"),
+    ];
+    displayedData.forEach((item) => {
+        if (item.classList.contains("item--checked")) {
+            item.classList.add("item--animation");
+            deleteItem(item);
+        }
+    });
+}
+function refreshCounter() {
+    let counter = 0;
+    if (toDoData) {
+        counter = toDoData.reduce((total, item) => {
+            return total + (item.isCompleted ? 0 : 1);
+        }, 0);
+    }
+    counterItem.innerText = `${counter} item${counter == 1 ? "" : "s"} left`;
+}
+// TOGGLE THEME - LIGHT (primary) / DARK
+const themeToggle = document.querySelector(".js-theme-toggle");
+themeToggle &&
+    themeToggle.addEventListener("click", () => {
+        document.body.classList.toggle("dark-theme");
+    });
